d3790dc717ac2a2e2e9574817f42337fafa703f8
Pratham45201 <prathampatel45201@gmail.com>
2025-01-20 23:21:15 +0530

    Initialized Project

---
+++ src/App.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+import { createRoot } from "react-dom/client";
+
+
+const App = () => {
+  return <div>Hello</div>;
+};
+
+const container = document.getElementById("root");
+const root = createRoot(container);
+root.render(<App />);
